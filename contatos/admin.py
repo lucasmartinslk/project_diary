f13ba66107a794b621d552318a001ddb61dd705e
Lucas Martins <lucas1993x@hotmail.com>
2020-10-01 16:19:17 -0300

    add admin configs

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome','sobrenome')
+    list_display_links = ('nome', 'sobrenome')
+
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
+
--- contatos/models.py
+++ contatos/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=200)
     sobrenome = models.CharField(max_length=200, blank=True)
@@ -14,4 +17,6 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.nome
 
